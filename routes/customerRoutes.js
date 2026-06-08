6c62947c0b0b133deb5aa10589a772c915da75ee
Vincentius Dylan <vincentiusdylan01@gmail.com>
2026-06-08 18:06:30 +0700

    refactor: fix router for add and check customer phone number

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 const protect = require('../middlewares/authMiddleware');
 
-router.post('/check', protect, customerController, checkCustomer);
-router.post('/', protect, customerController, addCustomer);
+router.post('/', protect, customerController.addCustomer);
+router.get('/check', protect, customerController.checkCustomer);
 router.get('/simple', protect, customerController.getCustomers);
 
 router.get('/', protect, customerController.getCustomersDashboard);
