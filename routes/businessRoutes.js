70a4b874c32dc774e2922b862622f6f3cd9ef4f4
Vincentius Dylan <vincentiusdylan01@gmail.com>
2026-06-10 18:04:16 +0700

    feat: add business controller and routes for uploading store logo to GCS

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -3,10 +3,21 @@ const router = express.Router();
 const businessController = require('../controllers/businessController');
 const protect = require('../middlewares/authMiddleware');
 
+const multer = require('multer');
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 2 * 1024 * 1024 }
+});
+
 router.post('/categories', protect, businessController.createCategory);
 router.post('/services', protect, businessController.createService);
 router.get('/menu', protect, businessController.fetchMenu);
 router.get('/', protect, businessController.fetchProfile);
-router.post('/profile', protect, businessController.uploadStorePicture);
+router.put(
+    '/profile/picture',
+    authenticateMiddleware,
+    upload.single('profile_picture'),
+    businessController.uploadStorePicture
+);
 
 module.exports = router;
